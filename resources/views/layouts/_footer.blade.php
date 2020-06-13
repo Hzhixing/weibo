5e0bff061de4634770befff59e7c17b8b60e399d
vagrant <vagrant@homestead>
2020-06-13 14:35:42 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=681487761,4057781657&fm=26&gp=0.jpg">
+  <a href="https://laravel-china.org/courses" target=_blank>
+    刻意练习，每日精进
+  </a>
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
