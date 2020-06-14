713d7980e77bc273ef9492b85487690a524883dc
vagrant <vagrant@homestead>
2020-06-14 09:36:52 +0000

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -12,18 +12,27 @@
 
       <form method="POST" action="{{ route('login') }}">
         {{ csrf_field() }}
+
         <div class="form-group">
           <label for="email">邮箱：</label>
           <input type="text" name="email" class="form-control" value="{{ old('email') }}">
         </div>
+
         <div class="form-group">
           <label for="password">密码：</label>
           <input type="password" name="password" class="form-control" value="{{ old('password') }}">
         </div>
+
+        <div class="form-group">
+          <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">记住我</label>
+        </div>
+
         <button type="submit" class="btn btn-primary">登录</button>
       </form>
 
       <hr>
+
       <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
     </div>
   </div>
