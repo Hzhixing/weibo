3240db21298b667578342c0362679cff744428d7
vagrant <vagrant@homestead>
2020-06-18 04:15:07 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -96,8 +96,6 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'summer@example.com';
-        $name = 'Summer';
         $to = $user->email;
         $subject = '感谢注册Weibo应用！请确认你的邮箱。';
 
